4db4792f389e9cd77b31f375c36477156053a7a1
Vestertech <vesteerwood@gmail.com>
2024-01-31 18:43:54 +0100

    React router and querying of project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import Clients from "./components/Clients";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
+
 const cache = new InMemoryCache({
   typePolicies: {
     Query: {
@@ -29,11 +32,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
